71a83c61da2d8adafc78e991281a5ea308be979b
Kevin Pennartz <47337620+KPennart@users.noreply.github.com>
2020-02-11 21:12:08 -0500

    Added function to handle player animated sprite and added wall sprite
    There is now a function to handle player animation. It's a fairly basic function that has a preset number of frames declared in love.load and it goes way too fast. Easy enough to fix.
    
    I also added a wall sprite for testing purposes.

--- Project Folder/main.lua
+++ Project Folder/main.lua
@@ -6,22 +6,34 @@ function love.load()
 	-- Store sprites into their own variables
 	box = love.graphics.newImage("Sprites/Box.png")
 	tileset = love.graphics.newImage("Sprites/Tileset.png")
+	playerSprite = love.graphics.newImage("Sprites/characterTest.png")
 	
 	-- Set the width and height of each tile on the screen (recommended to use power of 2 numbers)
 	tileWidth, tileHeight = 16, 16
+	playerWidth, playerHeight = 16, 16
 	-- Get the width and height of the tile spritesheet
 	local tilesetWidth, tilesetHeight = tileset:getWidth(), tileset:getHeight()
+	local playerSpriteWidth, playerSpriteHeight = playerSprite:getWidth(), playerSprite:getWidth()
 	
 	-- all tiles for the game are stored in quad, index starts at 1
 	quads = 
 	{
-		love.graphics.newQuad(0, 0, tileWidth, tileHeight, tilesetWidth, tilesetHeight) -- 1 = floor tile
+		love.graphics.newQuad(0, 0, tileWidth, tileHeight, tilesetWidth, tilesetHeight), -- 1 = floor tile
+		love.graphics.newQuad(16, 0	, tileWidth, tileHeight, tilesetWidth, tilesetHeight) -- 2 = wall tile
+	}
+	
+	playerSpriteSheet =
+	{
+		love.graphics.newQuad(0, 0, playerWidth, playerHeight, playerSpriteWidth, playerSpriteHeight), -- 1 = base sprite
+		love.graphics.newQuad(16, 0, playerWidth, playerHeight, playerSpriteWidth, playerSpriteHeight), -- 2 = right foot
+		love.graphics.newQuad(0, 16, playerWidth, playerHeight, playerSpriteWidth, playerSpriteHeight), -- 3 = base sprite again
+		love.graphics.newQuad(16, 16, playerWidth, playerHeight, playerSpriteWidth, playerSpriteHeight) -- 4 = left foot
 	}
 	
 	-- The game map is stored here, they are referenced by their quad index
 	tileTable = 
 	{
-		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -61,11 +73,13 @@ function love.load()
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
 	}
 	
-	player.img = box
+	player.isWalking = false
+	player.frame = 1
+	player.maxFrame = 4
 	
 	-- Variables that hold the width and height of the Box sprite in Sprites folder
-	playerWidth = player.img:getWidth()
-	playerHeight = player.img:getHeight()
+	--playerWidth = player.img:getWidth()
+	--playerHeight = player.img:getHeight()
 	
 	-- Place the character in the center of the screen
 	player.x = love.graphics.getWidth() / 2
@@ -80,30 +94,36 @@ end
 -- Update is run every frame
 -- Currently checks for movemovent input and moves the character if necessary
 function love.update(dt)
-
+	player.isWalking = false
+	
 	-- If the player is pressing d or a then move them horizontally
 	if love.keyboard.isDown('d') then
 		-- confine the player to the screen boundary
 		if player.x < (love.graphics.getWidth() - playerWidth) then
 			-- move the player if enough time has passed since the last check (dt)
 			player.x = player.x + (player.speed * dt)
+			player.isWalking = true
 		end
 	elseif love.keyboard.isDown('a') then
 		if player.x > 0 then
 			player.x = player.x - (player.speed * dt)
+			player.isWalking = true
 		end
 	end
 	
 	if love.keyboard.isDown('w') then
 		if player.y > 0 then
 			player.y = player.y - (player.speed * dt)
+			player.isWalking = true
 		end
 	elseif love.keyboard.isDown('s') then
 		if player.y < (love.graphics.getHeight() - playerHeight) then
 			player.y = player.y + (player.speed * dt)
+			player.isWalking = true
 		end
 	end
 	
+	playerAnimation()
 end
 
 function love.draw()
@@ -117,6 +137,9 @@ function love.draw()
 			love.graphics.draw(tileset, quads[number], (columnIndex-1) * tileWidth, (rowIndex - 1) * tileHeight)
 		end
 	end
+	
+	-- Debug message to display the current player frame
+	-- love.graphics.print(player.frame, 50, 50)
 
 	-- Local variables that store the current location of the mouse
 	local mouseX, mouseY = love.mouse.getPosition()
@@ -124,11 +147,27 @@ function love.draw()
 	local characterAngle = math.angle(mouseX, mouseY, player.x + (playerWidth / 2), player.y  + (playerHeight / 2))
 	
 	-- Draw the character with rotation applied
-	love.graphics.draw(player.img, player.x, player.y, characterAngle, 1, 1, 8, 8)
+	love.graphics.draw(playerSprite, playerSpriteSheet[player.frame], player.x, player.y, characterAngle, 1, 1, 8, 8)
 end
 
 -- Function takes two sets of coordinates and calculates the angle between the two points
 -- Returned angle assumes all sprites by default face left
 function math.angle(x1,y1, x2,y2)
 	return math.atan2(y2-y1, x2-x1)
+end
+
+-- Function that keeps track of player animation
+function playerAnimation()
+	-- If the player has pressed any of the movement keys
+	if player.isWalking then
+		-- Incremement the movement frame
+		player.frame = player.frame + 1
+		-- If the player's current frame is outside the range of possible frames, reset it to the base frame
+		if player.frame > player.maxFrame then
+			player.frame = 1
+		end
+	-- If the player has not pressed movement keys reset them to the base frame
+	else
+		player.frame = 1
+	end
 end
