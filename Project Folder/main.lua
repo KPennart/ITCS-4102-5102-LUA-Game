d23f314cf7629fb2e15d9cc7c56ffa04f194b1cb
tpneumann <tpneumann@uncc.edu>
2020-04-13 20:45:10 -0400

    enemies deal damage and u can die

--- Project Folder/main.lua
+++ Project Folder/main.lua
@@ -1,5 +1,21 @@
+
+-- adds gui library
+local uare = require "uare"
+
+
+-- variables controlling camera
+camera = { }
+camera.x = 0
+camera.y = 0
+camera.scaleX = 1
+camera.scaleY = 1
+camera.rotation = 0
+
 -- variable that stores player data such as position and speed
 player = { }
+
+--variable that holds if the game is over
+gameover = false
 
 -- variables that stores gun data
 playerBullets = { }	-- bullets in room
@@ -18,22 +34,16 @@ guTy = {slowboi = 1, jumpyboi = 2}
 -- guSt = states
 states = {still = 1, moving = 2}
 
--- variables controlling camera
-camera = { }
-camera.x = 0
-camera.y = 0
-camera.scaleX = 1
-camera.scaleY = 1
-camera.rotation = 0
 
-debugAngle1 = 0
-debugAngle2 = 0
 debugAngle3 = 0
 
 -- function is only called once at the beginning of the game
 function love.load()
+
+	WWIDTH, WHEIGHT = 1024, 576
+
 	-- Makes window resizable
-	love.window.setMode(1024, 576, {resizable=true, vsync=false, minwidth=1024, minheight=576})
+	love.window.setMode(WWIDTH, WHEIGHT, {resizable=true, vsync=false, minwidth=1024, minheight=576})
 
 	-- Store sprites into their own variables
 	tileset = love.graphics.newImage("Sprites/Tileset.png")
@@ -118,6 +128,9 @@ function love.load()
 		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
 	}
 
+	--sets up gui stuff
+	uareStuff()
+
 
 
 	--sets player default values
@@ -129,6 +142,9 @@ function love.load()
 	player.maxFrame = 4 * player.frameLength
 
 	player.health = 5
+
+	player.iframes = 0
+	player.iframesMax = .5
 	
 	-- Place the character in the center of the screen
 	player.x = love.graphics.getWidth() / 2
@@ -186,7 +202,7 @@ function love.load()
 	--holds the max time (in seconds) bullets can be on screen (1000 if not relevant)
 	guns[1].maxTime = 1000
 	guns[2].maxTime = 1000
-	guns[3].maxTime = 1
+	guns[3].maxTime = .5
 
 
 
@@ -276,11 +292,266 @@ function love.load()
 	
 end
 
+function uareStuff() 
+
+
+	healthStyle = uare.newStyle({
+		height = 25,
+		width = 30,
+
+		color = {1, 0, 0},
+
+		drag = {
+			enabled = true,
+			fixed = {
+				x = false,
+				y = true
+			},
+
+			bounds = {
+				{
+					x = WWIDTH*.05
+				},
+				{
+					x = WWIDTH*.25
+				}
+			}
+		}
+	})
+
+
+	healthBackgroundStyle = uare.newStyle({
+	    height = 25,
+	    width = WWIDTH*.25-21,
+	    
+	    color = {0, 1, 0},
+	    
+	    text = {
+			font = love.graphics.newFont(24),
+			color = {255, 255, 255},
+			align = "center",
+			offset = {
+	        	x = 150,
+	        	y = -16
+	    	}
+	    }
+	})
+
+	restartStyle = uare.newStyle({
+		height = 200,
+		width = 400,
+
+		color = {0, 0, 0},
+
+		text = {
+			font = love.graphics.newFont(24),
+			color = {1,1,1},
+			align = "center",
+			offset = {
+				x = 0,
+				y = -50
+			}
+		}
+
+	})
+
+	healthLabel = uare.new({
+		x = WWIDTH*.05,
+		y = WHEIGHT*.05
+	}):style(healthBackgroundStyle)
+
+	healthbar = uare.new({
+		x = WWIDTH*.25,
+		y = WHEIGHT*.05
+	}):style(healthStyle)
+
+	restartButton = uare.new({
+		x = WWIDTH * .5-200,
+		y = WHEIGHT * .5-100
+	}):style(restartStyle)
+	restartButton.text.display = "Game Over!\nPress R to Restart"
+
+end
+
 
 -- Update is run every frame
 -- Currently checks for movemovent input and moves the character if necessary
 function love.update(dt)
 
+	-- updates the GUI
+	updateUareStuff(dt)
+
+	-- only allow player movement if they have not already lost
+	if gameover == false then
+		updatePlayer(dt)
+
+	-- if game is over, allow player to hit r to restart
+	elseif love.keyboard.isDown('r') then
+		uare.clear()
+		love.event.quit("restart")
+	end
+	
+	if love.keyboard.isDown('escape') then
+		love.event.quit()
+	end
+	
+	-- playerAnimation()
+	playerFrameManager()
+
+	--update bullet positions
+	updateBullets(dt)
+	
+	--calculate remaining time of shot timers
+	checkShotTimers(dt)
+
+	--handle enemy updates
+	updateEnemies(dt)
+
+	--fire bullets if left click is held and we are allowed to shoot again
+	if gameover == false and love.mouse.isDown(1) and guns[gunType].shotTimer <= 0 then
+		fireBullets()
+	end
+
+	--put here in case of screen size change
+	widthOffset = love.graphics.getWidth() / 4
+	heightOffset = love.graphics.getHeight() / 4
+	
+	camera:setScale(0.5, 0.5)
+	camera:setPostion(player.x - widthOffset, player.y - heightOffset)
+	
+	
+end
+
+
+function love.draw()
+
+	camera:set()
+
+
+	-- For loop with second nested for loop that goes through the tileTable drawing each tile
+	-- in their respective x, y coordinates
+	for rowIndex = 1, #tileTable do
+		local row = tileTable[rowIndex]
+		for columnIndex = 1, #row do
+			local number = row[columnIndex]
+			love.graphics.draw(tileset, quads[number], (columnIndex-1) * tileWidth, (rowIndex - 1) * tileHeight)
+		end
+	end
+
+	-- Local variables that store the current location of the mouse
+	--local mouseX, mouseY = love.mouse.getPosition()
+	local mouseX, mouseY = camera:mousePosition()
+
+	-- Local variable that stores the angle that the character should be rotated to
+	local characterAngle = math.angle(mouseX, mouseY, player.x + (playerWidth / 2), player.y  + (playerHeight / 2))
+	if gameover == true then characterAngle = 0 end
+	playerSpriteDirection(characterAngle)
+
+	love.graphics.draw(playerSprite, playerSpriteSheet[player.frame + player.animationOffset], player.x, player.y, 0, 1, 1, 16, 16)
+	love.graphics.draw(playerGun, player.x, player.y+5, characterAngle, 1, 1, 26)
+	
+	--[[
+	love.graphics.print(characterAngle, 100, 100)
+	love.graphics.print(mouseX, 100, 112)
+	love.graphics.print(mouseY, 100, 124)
+	love.graphics.print(player.x, 100, 136)
+	love.graphics.print(player.y, 100, 148)
+	love.graphics.print(gunType, 100, 160)
+	love.graphics.print(debugAngle3, 100, 172)
+
+	-- love.graphics.print(player.animationOffset, 100, 112)
+	-- love.graphics.print(player.frame, 100, 124)
+
+	--]]
+
+	-- draws each instance of playerBullets
+	for j = 1, #playerBullets do
+		for i,v in ipairs(playerBullets[j]) do
+			local spr = guns[j].bulletSprite
+			love.graphics.draw(spr, v.x, v.y, 0, 1, 1, spr:getHeight()/2, spr:getWidth()/2)
+		end 
+	end
+
+	-- draws each enemy
+	for k = 1, #enemiesAround do
+		for j = 1, #enemiesAround[k] do
+			for i, v in ipairs(enemiesAround[k][j]) do
+				love.graphics.draw(enemies[k][j].enemySprite, v.x, v.y, 0, 1, 1, 0, 0)
+			end
+		end
+	end
+
+	-- TODO draws enemy bullets 
+
+
+
+	
+	
+	camera:unset()
+
+
+	-- draws the GUI, keep last so it draws over other stuff
+	--uare.draw()
+	healthLabel:drawSelf()
+	healthbar:drawSelf()
+	if (gameover == true) then restartButton:drawSelf() end
+	love.graphics.setColor(1,1,1)
+	
+	
+end
+
+-- Function takes two sets of coordinates and calculates the angle between the two points
+-- Returned angle assumes all sprites by default face left
+function math.angle(x1,y1, x2,y2)
+	return math.atan2(y2-y1, x2-x1)
+end
+
+-- function takes two sets of coordinates and finds the distance between them
+function findDistance(x1,y1, x2,y2) 
+	return math.sqrt((x2-x1)^2 + (y2-y1)^2)
+end
+
+--check if point (x, y) is in a box determined by c and s
+--(cx, cy) at top left
+--(cx + sx, cy + sy) at bottom left
+function collisionBox(x, y, cx, cy, sx, sy)
+	if (x > cx and x < cx + sx and y > cy and y < cy + sy) then
+		return true
+	end
+
+	return false
+end
+
+--check if two boxes are overlapping
+function collisionBoxes (x1, y1, x2, y2, cx, cy, sx, sy)
+	if x2 < cx or x1 > cx + sx or y2 < cy or y1 > cy + sx then
+		return false
+	end
+
+	return true
+end
+
+--iterates through guns the player has when right click is hit
+function love.mousepressed(x, y, button)
+	if button == 2 then
+	    gunType = gunType + 1
+
+	    --if the gun type is at the limit of the array, go back to start
+    	if gunType > #guns then gunType = 1 end
+	end
+end
+
+function updateUareStuff(dt)
+	uare.update(dt, love.mouse.getX(), love.mouse.getY())
+
+	healthbar:setHorizontalRange(player.health / 5)
+
+	local hp = healthbar:getHorizontalRange()
+
+	healthLabel.text.display = hp * 5
+end
+
+function updatePlayer(dt) 
 	player.isWalking = false
 	
 	-- If the player is pressing d or a then move them horizontally
@@ -309,133 +580,10 @@ function love.update(dt)
 			player.isWalking = true
 		end
 	end
-	
-	if love.keyboard.isDown('escape') then
-		love.event.quit()
-	end
-	
-	-- playerAnimation()
-	playerFrameManager()
 
-	--update bullet positions
-	updateBullets(dt)
-	
-	--calculate remaining time of shot timers
-	checkShotTimers(dt)
-
-	--handle enemy updates
-	updateEnemies(dt)
-
-	--fire bullets if left click is held and we are allowed to shoot again
-	if love.mouse.isDown(1) and guns[gunType].shotTimer <= 0 then
-		fireBullets()
-	end
-
-	--put here in case of screen size change
-	widthOffset = love.graphics.getWidth() / 4
-	heightOffset = love.graphics.getHeight() / 4
-	
-	camera:setScale(0.5, 0.5)
-	camera:setPostion(player.x - widthOffset, player.y - heightOffset)
-	
-	
-end
+	checkPlayerCollisions(player.x, player.y)
 
 
-function love.draw()
-
-
-	camera:set()
-
-
-	-- For loop with second nested for loop that goes through the tileTable drawing each tile
-	-- in their respective x, y coordinates
-	for rowIndex = 1, #tileTable do
-		local row = tileTable[rowIndex]
-		for columnIndex = 1, #row do
-			local number = row[columnIndex]
-			love.graphics.draw(tileset, quads[number], (columnIndex-1) * tileWidth, (rowIndex - 1) * tileHeight)
-		end
-	end
-
-	-- Local variables that store the current location of the mouse
-	--local mouseX, mouseY = love.mouse.getPosition()
-	local mouseX, mouseY = camera:mousePosition()
-
-	-- Local variable that stores the angle that the character should be rotated to
-	local characterAngle = math.angle(mouseX, mouseY, player.x + (playerWidth / 2), player.y  + (playerHeight / 2))
-	
-	playerSpriteDirection(characterAngle)
-	
-	love.graphics.draw(playerSprite, playerSpriteSheet[player.frame + player.animationOffset], player.x, player.y, 0, 1, 1, 16, 16)
-	love.graphics.draw(playerGun, player.x, player.y+5, characterAngle, 1, 1, 26)
-	
-	love.graphics.print(characterAngle, 100, 100)
-	love.graphics.print(mouseX, 100, 112)
-	love.graphics.print(mouseY, 100, 124)
-	love.graphics.print(player.x, 100, 136)
-	love.graphics.print(player.y, 100, 148)
-	love.graphics.print(gunType, 100, 160)
-	love.graphics.print(debugAngle3, 100, 172)
-
-	-- love.graphics.print(player.animationOffset, 100, 112)
-	-- love.graphics.print(player.frame, 100, 124)
-
-	-- draws each instance of playerBullets
-	for j = 1, #playerBullets do
-		for i,v in ipairs(playerBullets[j]) do
-			local spr = guns[j].bulletSprite
-			love.graphics.draw(spr, v.x, v.y, 0, 1, 1, spr:getHeight()/2, spr:getWidth()/2)
-		end 
-	end
-
-	-- TODO draws each enemy
-	for k = 1, #enemiesAround do
-		for j = 1, #enemiesAround[k] do
-			for i, v in ipairs(enemiesAround[k][j]) do
-				love.graphics.draw(enemies[k][j].enemySprite, v.x, v.y, 0, 1, 1, 0, 0)
-			end
-		end
-	end
-
-	-- TODO draws enemy bullets 
-	
-	
-	camera:unset()
-	
-	
-end
-
--- Function takes two sets of coordinates and calculates the angle between the two points
--- Returned angle assumes all sprites by default face left
-function math.angle(x1,y1, x2,y2)
-	return math.atan2(y2-y1, x2-x1)
-end
-
--- function takes two sets of coordinates and finds the distance between them
-function findDistance(x1,y1, x2,y2) 
-	return math.sqrt((x2-x1)^2 + (y2-y1)^2)
-end
-
---check if point (x, y) is in a box determined by c and s
---(cx, cy) at top left
---(cx + sx, cy + sy) at bottom left
-function collisionBox(x, y, cx, cy, sx, sy)
-	if (x > cx and x < cx + sx and y > cy and y < cy + sy) then
-		return true
-	end
-
-	return false
-end
-
---iterates through guns the player has when right click is hit
-function love.mousepressed(x, y, button)
-	if button == 2 then
-	    gunType = gunType + 1
-
-	    --if the gun type is at the limit of the array, go back to start
-    	if gunType > #guns then gunType = 1 end
-	end
 end
 
 -- function that manages what frame of the character table is displayed
@@ -485,8 +633,8 @@ function playerSpriteDirection(characterAngle)
 end
 
 function spawnEnemies() 
-
-	enemyCounts = { {1, 0, 0}, {0, 0} }
+	enemyCounts = { {math.random() * 6, math.random() * 3, math.random() * 2}, 
+	{0, 0} }
 
 	for k=1, #enemyCounts do
 		for j=1, #enemyCounts[k] do
@@ -511,8 +659,6 @@ function updateEnemies(dt)
 
 			local dis = findDistance(player.x, player.y, v.x, v.y)
 
-			debugAngle3 = dis
-
 			---[[
 			if v.state == states.still and dis < enemies[k][j].aggroRange then
 				v.state = states.moving
@@ -520,7 +666,6 @@ function updateEnemies(dt)
 
 			if v.state == states.moving then 
 				moveEnemy(v, enemies[k][j].moveSpeed, dt) 
-				debugAngle3 = 1
 			end
 			--]]
 		end 
@@ -555,13 +700,15 @@ function updateEnemies(dt)
 end
 
 function moveEnemy(v, spd, dt) 
-	local angle = math.atan2((player.y - v.y), (player.x - v.x))
+	if player.iframes <= 0 then
+		local angle = math.atan2((player.y - v.y), (player.x - v.x))
 
-	local enemyDx = spd * math.cos(angle)
-	local enemyDy = spd * math.sin(angle)
+		local enemyDx = spd * math.cos(angle)
+		local enemyDy = spd * math.sin(angle)
 
-	v.x = v.x + (enemyDx * dt)
-	v.y = v.y + (enemyDy * dt)
+		v.x = v.x + (enemyDx * dt)
+		v.y = v.y + (enemyDy * dt)
+	end
 end
 
 
@@ -601,6 +748,30 @@ function checkBounds(x, y)
 	return x < 0 or y < 0 or x > love.graphics.getWidth() or y > love.graphics.getHeight()
 end
 
+function checkPlayerCollisions(x, y) 
+	for k=1, #enemiesAround do
+		for j=1, #enemiesAround[k] do
+			for i,v in ipairs(enemiesAround[k][j]) do
+				--local dis = findDistance(x, y, v.x, v.y)
+				local spr = enemies[k][j].enemySprite
+
+				if player.iframes <= 0 and collisionBoxes(x-8, y-8, x+8, y+8,
+					v.x, v.y, spr:getWidth(), spr:getHeight()) then
+					player.health = player.health - 1
+					player.iframes = player.iframesMax
+					if (player.health <= 0) then
+						gameover = true
+					end
+
+					return true
+				end
+			end
+		end
+	end
+
+	return false
+end
+
 function checkPBulletCollisions(x, y)
 	for k=1, #enemiesAround do
 		for j=1, #enemiesAround[k] do
@@ -629,6 +800,10 @@ function checkShotTimers(dt)
 		if guns[i].shotTimer > 0 then
 			guns[i].shotTimer = guns[i].shotTimer - dt
 		end
+	end
+
+	if player.iframes > 0 then
+		player.iframes = player.iframes - dt
 	end
 end
 
